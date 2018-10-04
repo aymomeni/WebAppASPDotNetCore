68235538b8dc50b71ff99568ea66ca51ef902377
momenislc <aymomeni@gmail.com>
2018-10-04 13:50:13 -0600

    added jquery dependency, bootstrap (npm install bootstrap)

---
+++ DutchTreat/wwwroot/js/index.js
@@ -0,0 +1,15 @@
+﻿var x = 0;
+var s = "";
+
+alert("Hello Pluralsight");
+console.log("Got here. " + document.getElementById('theForm'));
+
+var theForm = document.getElementById('theForm');
+
+var button = document.getElementById('buyButton');
+button.addEventListener("click", function () {
+    console.log("Buying Item");
+});
+
+var productInfo = document.getElementsByClassName("product-props");
+//var listItems = productInfo.item[0].children;
