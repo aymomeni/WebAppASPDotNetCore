afac48a15c70d93cff5f7807ae5242c571e17606
momenislc <aymomeni@gmail.com>
2018-11-13 16:17:10 -0700

    added products as an injectable service

--- DutchTreat/ClientApp/app/app.module.ts
+++ DutchTreat/ClientApp/app/app.module.ts
@@ -1,17 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { ProductList } from './shop/productList.component';
+import { DataService } from './shared/dataService';
+
 @NgModule({
   declarations: [
       AppComponent,
       ProductList
   ],
   imports: [
-    BrowserModule
+      BrowserModule,
+      HttpClientModule
   ],
-  providers: [],
+  providers: [
+    DataService // allows for data to be injected
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
